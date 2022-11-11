52144c1bdd761966ffd28d65589f9f3e8dc75a08
Eric Yu <ericyu049@gmail.com>
2022-11-10 22:52:46 -0500

    DASHBOARD

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'dashboard-comp',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-    
+export class DashboardComponent implements OnInit{
+    today!: Date;
+    ngOnInit():void {
+        this.today = new Date();
+        setInterval(() => {
+            this.today = new Date();
+        }, 1000);
+    }
 }
